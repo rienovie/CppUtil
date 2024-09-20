99e0092a3aadedffbc3d9e8157bd6d39b0e9630e
Rienovie <fisher.vince@gmail.com>
2024-09-20 17:54:51 -0400

    update date format

--- util.cpp
+++ util.cpp
@@ -190,7 +190,7 @@ namespace util {
         struct tm tstruct;
         char buf[80];
         tstruct = *localtime(&now);
-        strftime(buf, sizeof(buf), "%Y-%m-%d.%X", &tstruct);
+        strftime(buf, sizeof(buf), "%Y-%m-%d-%H-%M-%S", &tstruct);
 
         return buf;
     }
